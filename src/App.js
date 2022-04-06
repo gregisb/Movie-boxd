e8b9633d6fa9cfd8edf0ed134724f0186a2c1cbc
Guilherme Regis <guilhermeb.regis@gmail.com>
2022-04-06 16:45:24 -0300

    creates Header component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Header } from './components/Header';
+import { Watchlist } from './components/Watchlist';
+import { Watched } from './components/Watched';
+import { Add } from './components/Add';
+
+import './lib/font-awesome/css/all.min.css';
 import './App.css';
 
 function App() {
   return (
-    <h1>Hello world</h1>
+    <Router>
+      <Header />
+
+      <Switch>
+        <Route exact path='/'>
+          <Watchlist />
+        </Route>
+
+        <Route path='/watched'>
+          <Watched />
+        </Route>
+
+        <Route path='/add'>
+          <Add />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
